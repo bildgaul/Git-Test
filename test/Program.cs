6e11650b7b045313b00a52db266ef0e36e5bb928
Billy Gauldin <bildgaul@ut.utm.edu>
2021-09-01 21:05:06 -0500

    Turned the program into a simple number guessing game. Notes/Venting: Without access to the needed commands (and some frustration learning what Read() does and how to roughly type convert) making this was pretty annoying. It's like giving a child roller skates and telling it to walk. They'll figure it out eventually (sometimes not) but it's painful to figure out. It did not help that there was not really any instruction on what each command was, and the source I did use, the Microsoft C# documents, went straight from teaching you how to print with Console.WriteLine to the kinds of types (classes and interfaces included), with me needing to Google everything past the basic print command.

--- test/Program.cs
+++ test/Program.cs
@@ -8,9 +8,35 @@ namespace test
 {
     class Program
     {
+        const int QUIT = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int tries = 0;
+            int input;
+            var rand = new Random();
+            int randNum = rand.Next(1, 101);
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 100. Enter " + QUIT + " to quit.");
+                input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(input);
+                if (input < 0 || input > 100)
+                {
+                    Console.WriteLine("Number out of bounds! Please enter a number between 1 and 100.");
+                }
+                else if (input > 0 && input < randNum)
+                {
+                    Console.WriteLine("Your guess was too low! Try again.");
+                    tries++;
+                }
+                else if (input < 100 && input > randNum)
+                {
+                    Console.WriteLine("Your guess was too high! Try again.");
+                    tries++;
+                }
+            } while (input != QUIT && input != randNum);
+            Console.WriteLine("The number was: " + randNum);
+            Console.WriteLine("Number of tries: " + tries);
         }
     }
 }
